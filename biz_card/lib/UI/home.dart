1ad9dcf622c42d496e73467023f4d5f51609e0aa
Awet <adimera00@gmail.com>
2022-01-13 18:43:26 +0100

    Initial commit

---
+++ biz_card/lib/UI/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ScaffoldExample extends StatelessWidget {...}
+
+class CustomButton extends StatelessWidget {
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+}
+
