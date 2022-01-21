9585a1b9a847b9b18edd5d45f24130fd551ad578
Awet <adimera00@gmail.com>
2022-01-21 14:59:27 +0100

    first co

--- awet_store/lib/main.dart
+++ awet_store/lib/main.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/material.dart';
-
-import 'Home.dart';
-
-void main() {
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home()));
-}
+// import 'package:flutter/material.dart';
+//
+// import 'Home.dart';
+//
+// void main() {
+//   runApp(MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Home()));
+// }
 
 
 
